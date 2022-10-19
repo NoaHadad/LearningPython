4fff9fa9d8645d49dea9243ab00d7530457b33b6
NoaHadad <34161861+NoaHadad@users.noreply.github.com>
2022-10-19 10:51:00 +0300

    Add files via upload

--- Decorator/test_decorator.py
+++ Decorator/test_decorator.py
@@ -6,16 +6,17 @@ def F(x: int, y: int) -> int:
     return x + y
 
 
-def test_counter():
-    f = F
-    assert f.counter == 0
-    f(0, 1)
-    assert f.counter == 1
-    f(0, 6)
-    assert f.counter == 2
+class TestCounterDecorator:
 
+    def test_counter(self):
+        f = F
+        assert f.counter == 0
+        f(0, 1)
+        assert f.counter == 1
+        f(0, 6)
+        assert f.counter == 2
 
-def test_call():
-    f = F
-    assert f(5, 1) == 6
-    assert f(9, 2) == 11
+    def test_call(self):
+        f = F
+        assert f(5, 1) == 6
+        assert f(9, 2) == 11
